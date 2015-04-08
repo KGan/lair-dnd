2e8e43e6782e16920eb8285c51ed8a2f7926d92c
Kevin Gan <lss.kevin.gan@gmail.com>
2015-04-07 20:59:49 -0700

    newrelic again

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,4 +3,18 @@ class Api::UsersController < ApplicationController
     @user = User.find(params[:id])
     render :show
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
